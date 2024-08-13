ee9afbcb426c7484560235b4f8842a0795e69690
PQSLam <pqslamglobal@gmail.com>
2024-08-12 22:49:20 -0400

    update scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
+        if (this.getAttribute('href') === './resume.html') {
+            // Allow the default behavior for the Resume link
+            return;
+        }
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
